c9adac4baff9d3c27844027d40df35bf8d1730cd
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-01-03 03:19:39 -0500

    created test spawn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use hyper::Response;
+
+async fn simulate_spawn() -> Result<(), std::io::Error> {
+    println!("starting thread...");
+    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+    println!("done!");
+    Ok(())
+}
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let task = tokio::spawn(async { simulate_spawn().await });
+    task.await.unwrap()
 }
